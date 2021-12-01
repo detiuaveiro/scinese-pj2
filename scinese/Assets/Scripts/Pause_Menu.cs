8a6357093a7ceddd20d2d28b18c79e58722fc3e4
Oliiveiira <sergiooliveira4820@gmail.com>
2021-12-01 13:18:10 +0000

    Primary pause menu.

---
+++ scinese/Assets/Scripts/Pause_Menu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_Menu : MonoBehaviour
+{
+    public static bool isGamePaused = false; //public static para poder aceder através de outros scripts
+    [SerializeField] GameObject pauseMenu; //mesma forma de ter uma variavel que possa ser vista no inspector, porém mais privada, pois só pode ser acedida através deste script
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) //Esc para pause
+        {
+            if (isGamePaused == true)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f; //Continuar tempo no jogo
+        isGamePaused = false;
+    }
+
+    private void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f; //Parar o tempo no jogo.
+        isGamePaused = true;
+    }
+
+    public void SettingsGame()
+    {
+        //Fazer outro panel para as settings
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();//Terminar jogo
+        Debug.Log("The game is over."); //Verificar se está a funcionar
+    }
+}
