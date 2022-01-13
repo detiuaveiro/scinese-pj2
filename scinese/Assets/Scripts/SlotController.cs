e43416ed7e8a8afd2edaac74b370cba4636e1ebf
Oliiveiira <sergiooliveira4820@gmail.com>
2022-01-13 17:24:19 +0000

    inventory spaces code.

--- scinese/Assets/Scripts/SlotController.cs
+++ scinese/Assets/Scripts/SlotController.cs
@@ -7,6 +7,14 @@ public class SlotController : MonoBehaviour
 
     private Player player;
     public int i;
+    public bool isSelected;
+    public Animator animator;
+    public GameObject slot2;
+    public GameObject slot3;
+    public GameObject slot4;
+
+    public Slot2 invslot2;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +25,68 @@ public class SlotController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.G))
+        if(Input.GetKeyDown(KeyCode.Alpha1))
         {
-            DropItem();
+            slot2.GetComponent<Animator>().SetBool("isSelected", false);
+            slot3.GetComponent<Animator>().SetBool("isSelected", false);
+            slot4.GetComponent<Animator>().SetBool("isSelected", false);
+           
+            isSelected = false;
+            if(isSelected == false)
+            {
+                isSelected = true;
+                animator.SetBool("isSelected", true);
+            } 
+        }else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            animator.SetBool("isSelected", false);
+            slot3.GetComponent<Animator>().SetBool("isSelected", false);
+            slot4.GetComponent<Animator>().SetBool("isSelected", false);
+            
+            isSelected = false;
+            if (isSelected == false)
+            {
+                isSelected = true;
+                slot2.GetComponent<Animator>().SetBool("isSelected", true);
+            } else if(isSelected == true)
+            {
+                if (Input.GetKeyDown(KeyCode.G))
+                {
+                    invslot2.DropItem();
+                }
+            }
+        }else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            animator.SetBool("isSelected", false);
+            slot2.GetComponent<Animator>().SetBool("isSelected", false);
+            slot4.GetComponent<Animator>().SetBool("isSelected", false);
+           
+            isSelected = false;
+            if (isSelected == false)
+            {
+                isSelected = true;
+                slot3.GetComponent<Animator>().SetBool("isSelected", true);
+            }
+        }else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            animator.SetBool("isSelected", false);
+            slot2.GetComponent<Animator>().SetBool("isSelected", false);
+            slot3.GetComponent<Animator>().SetBool("isSelected", false);
+            
+            isSelected = false;
+            if (isSelected == false)
+            {
+                isSelected = true;
+                slot4.GetComponent<Animator>().SetBool("isSelected", true);
+            }
+        }
+
+        if (isSelected == true)
+        {
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                DropItem();
+            }
         }
 
         if(transform.childCount <= 0)
@@ -27,6 +94,8 @@ public class SlotController : MonoBehaviour
             player.inventory.isSlotFull[i] = false;
         }
     }
+
+   
 
     public void DropItem()
     {
